293d6565e8bde895a60541d56201bc0ebdf6362e
NeeKo3 <leshaxx007@gmail.com>
2023-02-09 22:09:43 +0300

    refactoring

---
+++ src/core/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCU7J4ZZtld9-77LjitKKjwmxdK3rQHOXI",
+  authDomain: "react-messenger-71c87.firebaseapp.com",
+  projectId: "react-messenger-71c87",
+  storageBucket: "react-messenger-71c87.appspot.com",
+  messagingSenderId: "28158697880",
+  appId: "1:28158697880:web:5cb9e71bf4960ce42f8bed",
+  measurementId: "G-PBKM2DREL4",
+};
--- src/core/context/authContext.js
+++ src/core/context/authContext.js
@@ -3,16 +3,8 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getDoc, getFirestore, doc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import React, { createContext, useEffect, useState } from "react";
+import { firebaseConfig } from "../config/firebaseConfig";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCU7J4ZZtld9-77LjitKKjwmxdK3rQHOXI",
-  authDomain: "react-messenger-71c87.firebaseapp.com",
-  projectId: "react-messenger-71c87",
-  storageBucket: "react-messenger-71c87.appspot.com",
-  messagingSenderId: "28158697880",
-  appId: "1:28158697880:web:5cb9e71bf4960ce42f8bed",
-  measurementId: "G-PBKM2DREL4",
-};
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const firestore = getFirestore(app);
